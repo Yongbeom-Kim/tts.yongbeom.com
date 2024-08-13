38695cf8f5af1a780ed8aa3989c2400a904ff605
Yongbeom-Kim <dernbu@gmail.com>
2024-08-14 00:51:50 +0800

    fix: Fix docker backend!

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from os import environ
 
-load_dotenv('.env.prod')
+load_dotenv('.env')
 
 # print(environ.get('AWS_ACCESS_KEY_ID'))
 # print(environ.get('AWS_SECRET_ACCESS_KEY'))
