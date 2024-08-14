386a673728615895499dadafcada15b256f999ff
Yongbeom-Kim <dernbu@gmail.com>
2024-08-14 09:39:57 +0800

    fix: Vite build options to satisfy terraform dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, `${process.cwd()}/../env/`)
-  process.env = {...process.env, ...env};
+  const env = loadEnv(mode, `${process.cwd()}/../env/`);
+  process.env = { ...process.env, ...env };
   return {
     // vite config
+    build: {
+      rollupOptions: {
+        output: {
+          dir: 'dist',
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
